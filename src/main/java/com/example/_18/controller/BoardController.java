2bdda943e97c89830a0828eca8ff53cf7ced0f01
hongjm0912~ <s25036@gsm.hs.kr>
2026-05-11 08:55:31 +0900

    Railway 배포 설정

--- src/main/java/com/example/_18/controller/BoardController.java
+++ src/main/java/com/example/_18/controller/BoardController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/board")
+@CrossOrigin(origins = "*")
 public class BoardController {
 
     private final BoardService boardService;
